2672ee955bc759a60aa5d6e9e5120202da7b163b
Williams Aguilera <williams.aguilera@cloud-iq.com>
2019-02-26 18:21:00 +0100

    ADD: Comenzar a añadir material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +31,8 @@ import { TemperaturaCiudadComponent } from './temperatura-ciudad/temperatura-ciu
     FormsModule,
     HttpClientModule,
     ListaCiudadesModule,
-    SurfPipesModule
+    SurfPipesModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
